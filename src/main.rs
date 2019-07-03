2ba17457c8deec0b48886756707eb88a1f7b4972
Cesar Gusukuma <cesar.gusukuma@omni.com.br>
2019-07-03 14:04:54 -0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    middleware, web, App, HttpResponse, HttpServer,
+};
+
+fn index() -> HttpResponse {
+    HttpResponse::Ok().json("Hello World")
+}
+
+fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
+            .service(web::resource("/").route(web::get().to(index)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
 }
